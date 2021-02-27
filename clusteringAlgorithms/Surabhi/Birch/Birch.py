4058cfcb54657a047c11b9b0fd4d6aff26a132c5
Surabhi Gupta <surabhi2029@gmail.com>
2021-02-26 16:36:39 -0800

    Changes

--- clusteringAlgorithms/Surabhi/Birch/Birch.py
+++ clusteringAlgorithms/Surabhi/Birch/Birch.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pandas as pd 
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 sns.set()
 import csv
 from sklearn.datasets import make_blobs
 from sklearn.cluster import Birch
 
-df = pd.read_csv('../../data/cleanData/oneEncodedDiscord.csv')
+df = pd.read_csv('../../data/cleanData/OHE_PA.csv')
+df2 = pd.read_csv('../../data/cleanData/oneEncodedDiscord.csv')
 
 # ~~~~~ Loading data from csv separating based on commas ~~~~~
 data = []
-with open('../../data/cleanData/oneEncodedDiscord.csv') as csv_file:
+with open('../../data/cleanData/OHE_PA.csv') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=",")
 		line_count = 0
 
@@ -28,7 +30,7 @@ with open('../../data/cleanData/oneEncodedDiscord.csv') as csv_file:
 
 # ~~~~~ Filtering data and choosing which columns to plot ~~~~~
 # For friends and personal qualities
-filtered_data = np.array([[item[6], item[7]] for item in data])
+filtered_data = np.array([[item[2], item[3]] for item in data])
 filtered_data = np.array(filtered_data).astype(np.float64)
 # print('filtered data', filtered_data)
 
@@ -47,8 +49,8 @@ print("predictions", predictions)
 
 # ~~~~~ Unclustered scatter plot ~~~~~
 facet = sns.lmplot(
-		x="friend_qualities", 
-		y="personal_qualities", 
+		x="Business", 
+		y="Sciences", 
 		fit_reg=False, 
 		legend=True, 
 		legend_out=True, 
@@ -60,14 +62,33 @@ labels = np.reshape(predictions, (1, predictions.size))
 fdata = np.concatenate((filtered_data, labels.T), axis=1)
 print('fdata---', fdata)
 
-fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1)
-ax = fig.add_subplot(111)
-colors = fdata[:, 2]
-scatter = ax.scatter(fdata[:,0], fdata[:, 1], c=colors, s=50)
-ax.set_title("Clusters")
-ax.set_xlabel("friends_qualities")
-ax.set_ylabel("personal_qualities")
-# plt.colorbar(scatter)
+# fig = plt.figure()
+# # ax = fig.add_subplot(1, 1, 1)
+# ax = fig.add_subplot(111)
+# colors = fdata[:, 2]
+# scatter = ax.scatter(fdata[:,0], fdata[:, 1], c=colors, s=50)
+# ax.set_title("Clusters")
+# ax.set_xlabel("friends_qualities")
+# ax.set_ylabel("personal_qualities")
+# # plt.colorbar(scatter)
+# plt.show()
+
+sns.set_style('whitegrid')
+sns.countplot(x=df.index, hue='Business', data=df,palette='RdBu_r')
 plt.show()
 
+# random data
+x = np.random.randint(0,1+1)
+
+fig, ax = plt.subplots()
+
+# define the colors
+cmap = mpl.colors.ListedColormap(['r', 'k'])
+
+# create a normalize object the describes the limits of
+# each color
+bounds = [0., 0.5, 1.]
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+# plot it
+ax.imshow(x, interpolation='none', cmap=cmap, norm=norm)
--- data/OHEdiscord.py
+++ data/OHEdiscord.py
@@ -1,13 +1,13 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv('rawData/modifiedDiscord.csv')
 le = LabelEncoder()
-conditions =  ['major_department', 'year', 'music_genre', 'living', 
-'humour', 'sports','friday_night','school_balance','entertainment_genre']
-for i in range (len(conditions)):
-  data[conditions[i]] = le.fit_transform(data[conditions[i]])
+conditions = ['major_department', 'year', 'music_genre', 'living',
+              'humour', 'sports', 'personal_qualities', 'friend_qualities',
+              'friday_night', 'school_balance', 'entertainment_genre']
+for i in range(len(conditions)):
+    data[conditions[i]] = le.fit_transform(data[conditions[i]])
 
 data.to_csv('cleanData/oneEncodedDiscord.csv')
-
